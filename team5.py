182e0b5a31ec61f44387bbd486346c5e445be4a9
sahomann <sahomann@bps.k12.in.us>
2016-10-10 10:49:51 -0400

    Samuel

--- team5.py
+++ team5.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Team 5' # Only 10 chars displayed.
+strategy_name = 'collude'
+strategy_description = 'Try and get get the least amount of '
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
